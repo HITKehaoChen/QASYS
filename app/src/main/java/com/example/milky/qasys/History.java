10964d130bf1cdea8f7a7b03e0d76f767e29dd32
littlecoke <tgyzckh@hotmail.com>
2018-07-02 10:55:22 +0800

    Layout fixed && nothing...

--- app/src/main/java/com/example/milky/qasys/History.java
+++ app/src/main/java/com/example/milky/qasys/History.java
@@ -3,8 +3,7 @@ package com.example.milky.qasys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class History extends AppCompatActivity {
-
+public class History extends AppCompatActivity { //将qaactivity中的结果传入这里并显示.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
